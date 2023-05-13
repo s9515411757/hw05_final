5b7c9652beb20a077f818cd44bb270ac0e4d11a8
Александр Ершов <s9515411757@gmail.com>
2023-05-13 13:42:40 +0300

    Спринт 6

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -37,9 +37,8 @@ def profile(request, username):
     author = User.objects.get(username=username)
     post_list = author.posts.select_related('group')
     page_obj = pagination(post_list, request)
-    following = (
-            request.user.is_authenticated
-            and Follow.objects.filter(user=request.user).exists()
+    following = (request.user.is_authenticated and Follow.objects.filter(
+        user=request.user).exists()
     )
     context = {
         'page_obj': page_obj,
